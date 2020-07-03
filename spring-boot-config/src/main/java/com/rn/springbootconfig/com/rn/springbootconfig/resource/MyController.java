27521bd2bf08cbfe59ae054bc3ef3ae0f569ded8
Roshan Nemad <nemadroshan@gmail.com>
2020-07-04 00:49:26 +0530

    learned about profiles & Enviornment obj

--- spring-boot-config/src/main/java/com/rn/springbootconfig/com/rn/springbootconfig/resource/MyController.java
+++ spring-boot-config/src/main/java/com/rn/springbootconfig/com/rn/springbootconfig/resource/MyController.java
@@ -2,9 +2,11 @@ package com.rn.springbootconfig.com.rn.springbootconfig.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -26,8 +28,16 @@ public class MyController {
     @Value("#{${msg.map}}")
     private Map<String,String> myMap;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/greet")
     public String greeting() {
         return " "+msg+"------"+msg2+"---------"+myList+"------"+myMap+"-----------"+demoConfig;
+    }
+
+    @GetMapping("/env")
+    public String getEnv(){
+        return " "+ Arrays.toString(environment.getActiveProfiles());
     }
 }
